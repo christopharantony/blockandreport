cbe4b631ccc10f68996008bfa1f6de6e50ae092c
Christophar Antony <christopharantony@gmail.com>
2022-07-23 02:30:13 +0530

    Add Cloud Database

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express")
 const app = express()
 
 const mongoose = require("mongoose")
+require("dotenv").config();
 
-
-mongoose.connect("mongodb://localhost:27017/api", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGOURL, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
 }).then(() => {
     console.log("Connected to MongoDB")
 }).catch((err) => {
